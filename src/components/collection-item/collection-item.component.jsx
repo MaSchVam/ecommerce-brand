ecab08359338db15925b7ed7290c5c2c0c0261f5
MaSchVam <62725224+MaSchVam@users.noreply.github.com>
2020-04-17 13:52:31 -0700

    Updated shop data with new data, updated cart utils to group items in cartReducer, to avoid duplicate items in the cart. Added a quantity attribute to cart items in the addItemToCart utility function

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './collection-item.styles.scss';
 
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
     <div className='collection-item'>
         <div
             className='image'
@@ -16,7 +21,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
                 <span className='price'>{price}</span>
 
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}> Add to cart </CustomButton>
         </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,11 @@
 import CartActionTypes from './cart.types';
+import cartReducer from './cart.reducer';
 
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
 });
