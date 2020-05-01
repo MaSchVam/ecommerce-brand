c75fcaa1045d465dc6a17a2f62049a83c00b312c
MaSchVam <62725224+MaSchVam@users.noreply.github.com>
2020-04-30 21:41:37 -0700

    Integrated Stripe into our application and our frontend now accepts payments.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -32,8 +33,13 @@ const CheckoutPage = ({cartItems, total}) => (
             ))
         }
         <div className="total">
-            <span>TOTAL ${total}</span>
-        </div>
+            <span>TOTAL: ${total}</span></div>
+            <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={total} />
     </div>
 );
 
